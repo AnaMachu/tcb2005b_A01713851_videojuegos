4e2da46df900a3eff0a5bb4bbc9496c252480cfc
AnaMachu <machucamiranda22@gmail.com>
2026-03-02 20:14:28 -0600

    Ejercicio de Algebra relacional y llaves

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
-console.log("hola desde node");
-const filesystem = require('fs');
-filesystem.writeFileSync('hola.txt','hola desde node');
 
-const arreglo=[5000,60,90,100,10,20,1000,0,120,200,340,10000,50];
-for (let item of arreglo){
-    setTimeout(()=>{
+//console.log("hola desde node");
+//const filesystem = require('fs');
+//filesystem.writeFileSync('hola.txt','hola desde node');
+
+//const arreglo=[5000,60,90,100,10,20,1000,0,120,200,340,10000,50];
+//for (let item of arreglo){
+    //setTimeout(()=>{
         console.log(item);
-    },item);
-}
+    //},item);
+//}
 
-const http = require('http');
+//const http = require('http');
 
 const server = http.createServer((request,response) =>{
     //console.log(request);
     //console.log(request.url);
-    response.end()
+    //console.log(response)
+    //response.end()
+    //response.setHeader
 
 });
 
 server.listen(3000);
+
+//const html()
+
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+});
+
+app.listen(3000);
+                            
