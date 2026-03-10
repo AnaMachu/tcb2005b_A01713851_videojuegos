2b9a7842af6f24825717672775c8e03348fa20b5
AnaMachu <machucamiranda22@gmail.com>
2026-03-09 23:22:43 -0600

    lab 2 listo

--- npmclass.js
+++ npmclass.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const app = express();
 
-const path=require(path);
-app.use(express.static(path.join(__dirname,'public'))); //daarle acceso a todo el mundo
+const path = require('path');  
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine','ejs');
-app.set('views','views');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
-const rutasVideojuegos = require('../routes/videojuegos.routes');
-app.use('/videojuegos', rutasVideojuegos);
+const rutaslab11 = require('./routes/lab11.routes');
+app.use('/lab11', rutaslab11);
+const rutasmod2 = require('./routes/module211.routes');
+app.use('/module211', rutasmod2);
 
-app.use((request,response, nexto)=>{
-  response.status(404).send("El videojuego no existe");
-})
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next();
+});
+
+app.use((request, response, next) => {
+  response.status(404).send("La página no existe");
+});
 
 app.listen(3000);
--- routes/module211.routes.js
+++ routes/module211.routes.js
@@ -1,51 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
-const html_header = `
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Información</title>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
-</head>
-<body>
-<section class="section">
-<div class="container">
-`;
-
-const html_footer = `
-</div>
-</section>
-</body>
-</html>
-`;
-
-
-
-router.get('/', (req, res) => {
-    res.send(html_header + `
-        <h1 class="title">Información del sitio</h1>
-        <p>Este sitio permite registrar razas de perros.</p>
-    ` + html_footer);
+router.get('/', (request, response, next) => {
+    response.render('info/index');
 });
-
-
-
-router.get('/autor', (req, res) => {
-    res.send(html_header + `
-        <h2 class="title">Autor</h2>
-        <p>Ana Valeria Machuca Miranda</p>
-    ` + html_footer);
+router.get('/preguntas', (request, response, next) => {
+    response.render('info/preguntas');
 });
-
-
-
-router.get('/contacto', (req, res) => {
-    res.send(html_header + `
-        <h2 class="title">Contacto</h2>
-        <p>Email: ejemplo@email.com</p>
-    ` + html_footer);
+router.get('/contacto', (request, response, next) => {
+    response.render('info/contacto');
 });
 
 module.exports = router;
